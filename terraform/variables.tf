dffc5f19a5431865bcdbb94907d34b527da9f003
Muhammad Shayan Shahid <bcsbs2212325@szabist.pk>
2025-06-14 18:20:04 +0500

    Add variables.tf and update .gitignore

---
+++ terraform/variables.tf
@@ -0,0 +1,6 @@
+variable "subscription_id" { default = "" }
+variable "client_id"       { default = "" }
+variable "client_secret"   { default = "" }
+variable "tenant_id"       { default = "" }
+variable "location"        { default = "East US" }
+variable "vm_name"         { default = "demo-vm" }
